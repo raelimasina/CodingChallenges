7a228695782c46990759490c5fa8450b9c4375bf
rmasina <raeli.masina@gmail.com>
2022-09-13 16:50:52 -0600

    added 2022-09-13 CC

---
+++ jsChallenges/cc_2022-09-13.js
@@ -0,0 +1,19 @@
+function basicOp(operation, value1, value2)
+{
+  let result = 0;
+  switch (operation){
+      case '+':
+        result = value1 + value2;
+        break;
+      case '-':
+        result = value1 - value2;
+        break;
+      case '*':
+        result = value1 * value2;
+        break;
+      case '/':
+        result = value1 / value2;
+        break;
+  }
+  return result;
+}
