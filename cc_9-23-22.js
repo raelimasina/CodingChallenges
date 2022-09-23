985dcca5dff12ae9b0e337bc29e6c03b859581e2
rmasina <raeli.masina@gmail.com>
2022-09-23 15:14:40 -0600

    coding challenge for 9-23-22

---
+++ cc_9-23-22.js
@@ -0,0 +1,7 @@
+function makeNegative(num) {
+    //check if num is already negative
+    //if the num is not negative, multiply by -1 and return it
+    //if the num is negative, return it
+    num = -Math.abs(num);
+    return num;
+  }
