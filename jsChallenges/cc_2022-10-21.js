584943ce52bbd68eafeefe4d1bcb9332791a1db9
Raeli Masina <raeli.masina@gmail.com>
2022-10-21 14:19:53 -0600

    Update cc_2022-10-21.js

--- jsChallenges/cc_2022-10-21.js
+++ jsChallenges/cc_2022-10-21.js
@@ -5,6 +5,31 @@
  */
 var containsNearbyDuplicate = function(nums, k) {
     let check=false;
+    if(1<=nums.length && nums.length <= Math.pow(10,5)){
+        for(let i=0; i<nums.length; i++){
+            if(-Math.abs(Math.pow(10,9)) <= nums[i] && nums[i]<= Math.pow(10,9)){
+                for(let j=0; j<nums.length; j++){
+                    if(j!==i && nums[i]===nums[j] && 0<=k && k<=Math.pow(10,5) && Math.abs(i-j)<=k){
+                        check = true;
+                    };
+                };
+            };
+        }; 
+    };
+    
+    return check;
+};
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let check=false;
     //nested loops checking that indeces i and j are ===
     for(let i=0; i<nums.length; i++){
         for(let j=0; j<nums.length; j++){
