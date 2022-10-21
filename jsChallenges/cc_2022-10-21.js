89b96e1f0a0ecc991304989df2fe4d5d88461711
Raeli Masina <raeli.masina@gmail.com>
2022-10-21 15:56:26 -0600

    Update cc_2022-10-21.js

--- jsChallenges/cc_2022-10-21.js
+++ jsChallenges/cc_2022-10-21.js
@@ -1,15 +1,48 @@
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
+ * 
+ * Below worked, but then encountered runtime error
+ * javascript heap out of memory
  */
 var containsNearbyDuplicate = function(nums, k) {
     let check = [];
     //get all indexes of what is in each position.
     for(let i=nums.length-1; i>0; i--){
         let indexes = getAllIndexes(nums, nums[i]);
+        for(let j = indexes.length-1; j>0; j--){
+            if(indexes[j]-indexes[j-1]<=k){
+                check.push(true);
+            }
+        }
+    }
+    return check.length>0;
+};
+function getAllIndexes(arr, val){
+    const indexes=[];
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]===val){
+            indexes.push(i);
+        }   
+    }
+    return indexes;
+}
+
+
+
+
+
+/**
+ * the following is my second attempt where I 
+ * used indexes
+ * Wrong answer
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let check = [];
+    //get all indexes of what is in each position.
+    for(let i=nums.length-1; i>0; i--){
+        let indexes =[];
+        indexes = getAllIndexes(nums, nums[i]);
         console.log(indexes)
-        if(indexes.length >1 && Math.abs(indexes[1]-indexes[indexes.length-1])<=k){
+        if(indexes.length >1 && Math.abs(indexes[0]-indexes[indexes.length-1])<=k){//!need to check distance of every duplicate
             check.push(true);
         }
     }
@@ -20,8 +53,7 @@ function getAllIndexes(arr, val){
     for(let i = 0; i<arr.length; i++){
         if(arr[i]===val){
             indexes.push(i);
-        }
-        
+        }   
     }
     return indexes;
 }
@@ -30,11 +62,11 @@ function getAllIndexes(arr, val){
 
 
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
+ * the following is my first attempt where I 
+ * used indexes so didn't have to run into nested loops making it run so many times.
+ * Wrong answer
  */
-var containsNearbyDuplicate = function(nums, k) {
+var containsNearbyDuplicate4 = function(nums, k) {
     let check = false;
     //get all indexes of what is in each position.
     for(let i=nums.length-1; i>0; i--){
@@ -60,11 +92,11 @@ function getAllIndexes(arr, val){
 
 
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
+ * below I added the constraints listed in the problem description to try and help reduce processing time
+ * 
+ * Time limit exceeded
  */
-var containsNearbyDuplicate = function(nums, k) {
+var containsNearbyDuplicate3 = function(nums, k) {
     let check=false;
     if(1<=nums.length && nums.length <= Math.pow(10,5)){
         for(let i=0; i<nums.length; i++){
@@ -76,8 +108,7 @@ var containsNearbyDuplicate = function(nums, k) {
                 };
             };
         }; 
-    };
-    
+    }; 
     return check;
 };
 
@@ -85,11 +116,11 @@ var containsNearbyDuplicate = function(nums, k) {
 
 
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
+ * works, but...
+ * 
+ * Time limit exceeded
  */
-var containsNearbyDuplicate = function(nums, k) {
+var containsNearbyDuplicate2 = function(nums, k) {
     let check=false;
     //nested loops checking that indeces i and j are ===
     for(let i=0; i<nums.length; i++){
@@ -103,15 +134,12 @@ var containsNearbyDuplicate = function(nums, k) {
 };
 
 
-
-
-
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
+ * first attempt at doing Contains Duplicates II leetcode problem
+ * 
+ * wrong answer
  */
-var containsNearbyDuplicate = function(nums, k) {
+var containsNearbyDuplicate1 = function(nums, k) {
     let check1=false;
     let check2=false;
     //nested loops checking that indeces i and j are ===
@@ -127,6 +155,5 @@ var containsNearbyDuplicate = function(nums, k) {
             }
         }
     }
-    return check2
-    
+    return check2;  
 };
