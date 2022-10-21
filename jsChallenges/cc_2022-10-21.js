5106b6d86c04776e41bc06f25bfc10b6c6556b61
Raeli Masina <raeli.masina@gmail.com>
2022-10-21 09:29:58 -0600

    Update cc_2022-10-21.js

--- jsChallenges/cc_2022-10-21.js
+++ jsChallenges/cc_2022-10-21.js
@@ -4,6 +4,28 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
+    let check=false;
+    //nested loops checking that indeces i and j are ===
+    for(let i=0; i<nums.length; i++){
+        for(let j=0; j<nums.length; j++){
+            if(j!==i && nums[i]===nums[j] && Math.abs(i-j)<=k){
+                    check = true;
+            }
+        }
+    }
+    return check;
+};
+
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
     let check1=false;
     let check2=false;
     //nested loops checking that indeces i and j are ===
