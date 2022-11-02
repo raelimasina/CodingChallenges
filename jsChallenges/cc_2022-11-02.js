f0523dd737919127532927f845de88a6cb5fd2da
Raeli Masina <raeli.masina@gmail.com>
2022-11-02 13:04:56 -0600

    Create cc_2022-11-02.js

---
+++ jsChallenges/cc_2022-11-02.js
@@ -0,0 +1,5 @@
+//debugging. this code does not execute properly. Try to figure out why.
+function multiply(a, b){
+  //original code a * b did not have a return, so resulted in undefined.
+  return a * b
+}
