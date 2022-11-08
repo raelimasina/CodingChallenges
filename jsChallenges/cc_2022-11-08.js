06b709fdd5c608fea9f8628586fb2ca8e25813ad
Raeli Masina <raeli.masina@gmail.com>
2022-11-08 12:08:09 -0700

    Update cc_2022-11-08.js

--- jsChallenges/cc_2022-11-08.js
+++ jsChallenges/cc_2022-11-08.js
@@ -32,3 +32,62 @@ this.isWorthIt = function(){
     }
   }
 }
+
+
+//PARKING LOT
+//for this one, I got it to work on a shallow level, however could not get it to work where the system couldn't move vehicles.
+Bike.prototype.type = 'bike';
+Car.prototype.type = 'car';
+Van.prototype.type = 'van';
+
+
+class ParkingLot {
+  constructor(size) {
+    this.size = size;
+    this.availableSpaces = size;
+    //this.vehiclesInLot = [];
+    this.bikes = [];
+    this.cars = [];
+    this.vans = [];
+  }
+  park(vehicle) {
+    if(vehicle.type === 'bike' && this.availableSpaces >0){
+      //this.vehiclesInLot.push(vehicle.license);
+      this.bikes.push(vehicle.license);
+      this.availableSpaces -= 1; 
+      return true;
+    }else if(vehicle.type === 'car' && this.availableSpaces>=2){
+     // this.vehiclesInLot.push(vehicle.license);
+      this.cars.push(vehicle.license);
+      this.availableSpaces -= 2;
+      return true;
+    }else if(vehicle.type === 'van' && this.availableSpaces>=3){
+      //this.vehiclesInLot.push(vehicle.license);
+      this.vans.push(vehicle.license);
+      this.availableSpaces -= 3;
+      return true;
+    }else{
+      return false;
+    }
+  }
+  retrieve(license) {
+    const bikeIndex = this.bikes.indexOf(license);
+    const carIndex = this.cars.indexOf(license);
+    const vanIndex = this.vans.indexOf(license);
+    if(bikeIndex >= 0){
+      this.bikes.splice(bikeIndex,1);
+      this.availableSpaces +=1;
+      return true;
+    }else if (carIndex >= 0){
+      this.cars.splice(carIndex,1);
+      this.availableSpaces +=2;
+      return true;
+    }else if (vanIndex >= 0){
+      this.vans.splice(vanIndex,1);
+      this.availableSpaces +=3;
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
