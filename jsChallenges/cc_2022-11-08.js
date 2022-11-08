ae2aadef89da8c0861107194c46f36e610417204
Raeli Masina <raeli.masina@gmail.com>
2022-11-08 11:16:51 -0700

    Update cc_2022-11-08.js

--- jsChallenges/cc_2022-11-08.js
+++ jsChallenges/cc_2022-11-08.js
@@ -1,7 +1,8 @@
+//Today I decided to solidify my understanding of OOP with several OOP coding challenges
+
+//GRINCH STOLE CHRISTMAS
 //need to store original method in a variable so that it can be used
 const originalDate = Date.prototype.getDate;
-
-
 //then can modify the method
 Date.prototype.getDate = function getGrinchDate() {
   //store the original date in a variable by calling the original method
@@ -12,5 +13,22 @@ Date.prototype.getDate = function getGrinchDate() {
     return 26;
   }else{
     return date;
+  }
+}
+
+
+//FATPHOBIC PIRATES
+function Ship(draft,crew) {
+ this.draft = draft;
+ this.crew = crew;
+  
+this.isWorthIt = function(){
+    const crewDraft = this.crew*1.5;
+    const loot = this.draft - crewDraft;
+    if(loot > 20){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
